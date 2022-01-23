955ff560346af9571e27dc96f0d644ebb451d845
perunin <perunin@icloud.com>
2022-01-23 16:02:21 +0300

    1.6. Взаимодействие объектов [#166087] v5

--- src/main/java/ru/job4j/tracker/Ball.java
+++ src/main/java/ru/job4j/tracker/Ball.java
@@ -2,9 +2,9 @@ package ru.job4j.tracker;
 
 public class Ball {
     public void tryRun(boolean condition) {
-        if (true) {
+        if (condition) {
             System.out.println("Колобок съеден");
-        } else if (false) {
+        } else {
             System.out.println("Колобок сбежал");
         }
     }
