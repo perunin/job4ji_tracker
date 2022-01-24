286904a1645e1b76fd1c76dbc1faa0dd3e4a2937
perunin <perunin@icloud.com>
2022-01-24 15:52:12 +0300

    2.1. Рефакторинг - Расстояние между точками v2

---
+++ src/test/java/ru/job4j/tracker/PointTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class PointTest  {
+
+    @Test
+    public void whenX0Y0ThenDistance0() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        double rsl = a.distance(b);
+        double expected = 0;
+        assertThat(expected, is(a.distance(b)));
+    }
+
+    @Test
+    public void whenX0Y2ThenDistance2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double rsl = a.distance(b);
+        double expected = 2;
+        assertThat(expected, is(a.distance(b)));
+    }
+}
