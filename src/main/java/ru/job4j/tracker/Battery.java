bfb9e6cfe9bf8cdc74aad95b098f2666ac7f1dc6
perunin <perunin@icloud.com>
2022-01-24 09:45:57 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/tracker/Battery.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+
+    }
+
+    public int getLoad() {
+        return load;
+    }
+}
