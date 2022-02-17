a6521bd0392dbddcb6e2f573df87d1a9eb8c6cca
perunin <perunin@icloud.com>
2022-02-17 11:20:31 +0300

    Переопределение [#174890 #264391] v3

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,8 +3,9 @@ package ru.job4j.inheritance;
 public class JSONReport extends  TextReport {
 
     public String generate(String name, String body) {
-        name = "name";
-        body = "body";
-        return name + System.lineSeparator() + body;
+        return "{" +
+                System.lineSeparator() + "\t\"name\" : " + "\"" + name +"\"," +
+                System.lineSeparator() + "\t\"body\" : " + "\"" + body + "\"" +
+                System.lineSeparator() + "}";
     }
 }
